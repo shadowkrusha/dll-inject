1a2be4967619f2de2ddb05d71d44235c21f7845d
Flavien Normand <contact@flavien-normand.fr>
2024-10-11 10:22:35 +0200

    chore(deps): update node-gyp

--- injector.cc
+++ injector.cc
@@ -5,8 +5,6 @@ using namespace Nan;
 
 NAN_MODULE_INIT(InitAll) {
 	Set(target, New("injectPID").ToLocalChecked(), GetFunction(New<FunctionTemplate>(injectPID)).ToLocalChecked());
-	Set(target, New("isProcessRunning").ToLocalChecked(), GetFunction(New<FunctionTemplate>(isProcessRunning)).ToLocalChecked());
-	Set(target, New("isProcessRunningPID").ToLocalChecked(), GetFunction(New<FunctionTemplate>(isProcessRunningPID)).ToLocalChecked());
 	Set(target, New("getPIDByName").ToLocalChecked(), GetFunction(New<FunctionTemplate>(getPIDByName)).ToLocalChecked());
 }
 
